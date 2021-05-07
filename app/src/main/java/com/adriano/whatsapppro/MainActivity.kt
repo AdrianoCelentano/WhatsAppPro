2e42a0f9ddb514e635527ceba1ac9c11cd170de3
AdrianoCelentano <adrian.schaefer87@gmail.com>
2021-05-07 11:43:48 +0200

    list

--- app/src/main/java/com/adriano/whatsapppro/MainActivity.kt
+++ app/src/main/java/com/adriano/whatsapppro/MainActivity.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-class TwoListActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
