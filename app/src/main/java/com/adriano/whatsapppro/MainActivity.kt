992d75e672be98d86dd2ef7945f7014691d300ab
AdrianoCelentano <adrian.schaefer87@gmail.com>
2021-05-07 11:45:46 +0200

    state

--- app/src/main/java/com/adriano/whatsapppro/MainActivity.kt
+++ app/src/main/java/com/adriano/whatsapppro/MainActivity.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-class ThreeStateActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
